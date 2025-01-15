6b35e00c4544499c6f7ff89e4498e035340a8f37
Jonathan Hill <144107180+MrHighViolet@users.noreply.github.com>
2025-01-15 13:10:17 +0000

    added prolific table and image

--- spotify.py
+++ spotify.py
@@ -20,6 +20,22 @@ df = load_data("spotify_dataset.csv")
 english_songs = df[df["language"] == "English"]
 non_eng = df[df["language"] != "English"]
 
+top_prol_artist, shakar, col3, col4 = st.columns(4)
+
+top_artists = df['artist_name'].value_counts(sort=True).head()
+# Reset the index and add 1 to start the index at 1
+top_artists = top_artists.reset_index().rename(columns={'index': 'Artist', 'artist_name': 'Artist', 'count': 'Count'})
+top_artists.index = top_artists.index + 1
+
+# Add a title
+top_prol_artist.write("### Top 5 Most Prolific Artists")
+
+# Display the table
+top_prol_artist.table(top_artists)
+shakar.image('shankar_mahadevan.jpeg', width=200)
+
+col3.write('col3')
+col4.write('col4')
 
 @st.cache_data
 def year_to_decade(year):
@@ -27,9 +43,7 @@ def year_to_decade(year):
     decade = f"{decade}s"
     return decade
 
-
 df["decade"] = df["year"].apply(lambda x: year_to_decade(x))
-
 
 @st.cache_data
 def plot_songs_by_decade(df):
@@ -63,7 +77,6 @@ def plot_songs_by_decade(df):
     )
     ax.invert_xaxis()
     return fig
-
 
 @st.cache_data
 def plot_songs_by_language(df):
@@ -104,13 +117,11 @@ def plot_songs_by_language(df):
     subplot.invert_xaxis()
     return canv
 
-
 # Tableau embed code
 tableau_html = """
 <div class='tableauPlaceholder' id='viz1736934652251' style='position: relative'><noscript><a href='#'><img alt='Dashboard 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Sp&#47;Spotify_17362545262740&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Spotify_17362545262740&#47;Dashboard1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Sp&#47;Spotify_17362545262740&#47;Dashboard1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-GB' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1736934652251');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else { vizElement.style.width='100%';vizElement.style.height='727px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
 """
-
-# # Embed the Tableau visualization
+# Embed the Tableau visualization
 st.components.v1.html(tableau_html, height=1400)
 
 # Features line graph
@@ -197,13 +208,13 @@ if any(st.session_state.features_to_plot.values()):
 else:
     st.info("Please select at least one feature to display the plot.")
 
-col1, col2 = st.columns(2)
-col1.button("Clear All", key="clear_btn", on_click=clear_all)
-col2.button("Select All", key="select_btn", on_click=select_all)
+clear_col, select_col2 = st.columns(2)
+clear_col.button("Clear All", key="clear_btn", on_click=clear_all)
+select_col2.button("Select All", key="select_btn", on_click=select_all)
 
-# Create horizontal columns for checkboxes
+# horizontal columns for checkboxes
 num_features = len(audio_features)
-cols = st.columns(num_features)  # Creates one column per feature
+cols = st.columns(num_features)  # one column per feature
 
 # Create checkboxes in horizontal layout
 for idx, (feature, col) in enumerate(zip(audio_features, cols)):
