a37fe08d5e26c96b79e3917516860b7848b68862
Jonathan Hill <144107180+MrHighViolet@users.noreply.github.com>
2025-01-20 14:51:42 +0000

    added sidebar and expander for business case

--- spotify.py
+++ spotify.py
@@ -24,6 +24,9 @@ st.markdown('<h1 class="centered-title">Spotify Dataset Analysis</h1>', unsafe_a
 st.markdown('<div style="display: flex; flex-wrap: wrap; gap: 4px; justify-content: center;"><img src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54" height="32" alt="Python" style="margin-right: 4px"><img src="https://img.shields.io/badge/-Pandas-333333?style=flat&logo=pandas" height="32" alt="Pandas" style="margin-right: 4px"><img src="https://img.shields.io/badge/-Matplotlib-000000?style=flat&logo=python" height="32" alt="Matplotlib" style="margin-right: 4px"><img src="https://img.shields.io/static/v1?label=Powered%20by&message=seaborn&color=E523F5&style=flat" height="32" alt="Seaborn" style="margin-right: 4px"><img src="https://img.shields.io/badge/Jupyter-notebook-brightgreen" height="32" alt="Jupyter" style="margin-right: 4px"><img src="https://img.shields.io/badge/Tableau-E97627?style=for-the-badge&logo=Tableau&logoColor=white" height="32" alt="Tableau" style="margin-right: 4px"></div>', unsafe_allow_html=True)
 st.markdown('<h2 class="centered-header">Jonathan Hill</h2>', unsafe_allow_html=True)
 st.markdown('<p align="center"><a href="https://www.linkedin.com/in/jonathanburthill/" target="_blank"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" height="28" style="margin-right: 4px"></a> <a href="https://github.com/jonathandhill" target="_blank"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" height="28" style="margin-right: 4px"></a> <a href="mailto:jonathan.burt.hill@gmail.com" aria-label="Send an email to Jonathan Hill"><img src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white" height="28" style="margin-right: 4px"></a></p>', unsafe_allow_html=True)
+with st.expander('See business case & data issues'):
+    st.write('This project investigates how popular music has evolved over time through analysis of Spotify\'s audio feature data. Using metrics such as acousticness, danceability, energy, tempo, and valence, we\'ll examine trends in musical characteristics across different time periods. The analysis will focus on quantifying and visualizing changes in these musical attributes to understand how production styles and preferences have shifted. Our goal is to uncover meaningful patterns in how music has transformed, potentially revealing insights about changing cultural tastes and production techniques.')
+
 st.divider()
 
 @st.cache_data
@@ -37,7 +40,27 @@ df = load_data("spotify_dataset.csv")
 english_songs = df[df["language"] == "English"]
 non_eng = df[df["language"] != "English"]
 
-top_prol_artists_col, shakar, pop_artists_col, big_dawgs = st.columns(4, vertical_alignment="top", border=False)
+st.sidebar.write('### Statistics')
+total_songs = len(df)
+total_artists = df['artist_name'].nunique()
+year_range = f"{df['year'].min()} and {df['year'].max()}"
+most_songs_year = df['year'].value_counts().idxmax()
+least_songs_year = df['year'].value_counts().idxmin()
+
+st.sidebar.write(f'Total number of songs: {total_songs}')
+st.sidebar.write(f'Total number of artists: {total_artists}')
+st.sidebar.write(f'Between {year_range}')
+st.sidebar.write(f'Most songs written in {most_songs_year}, least in {least_songs_year}')
+
+st.sidebar.write('### Features')
+st.sidebar.write('*Acousticness* - A confidence measure of whether the track is acoustic.')
+st.sidebar.write('*Danceability* - How suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. ')
+st.sidebar.write('*Energy* - Represents a perceptual measure of intensity and activity. Features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, and general entropy.')
+st.sidebar.write('*Instrumentalness* - Predicts whether a track contains no vocals.')
+st.sidebar.write('*Liveness* - Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live.')
+st.sidebar.write('*Valence* - The musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry).')
+
+top_prol_artists_col, shakar, pop_artists_col, big_dawgs = st.columns(4, vertical_alignment="center", border=False)
 
 @st.cache_data
 def get_top_prolific_artists(df):
@@ -45,6 +68,47 @@ def get_top_prolific_artists(df):
     top_prol_artists = top_prol_artists.reset_index().rename(columns={'index': 'Artist', 'artist_name': 'Artist', 'count': 'Count'})
     top_prol_artists.index = top_prol_artists.index + 1
     return top_prol_artists
+
+@st.cache_data
+def year_to_decade(year):
+    decade = (year // 10) * 10
+    decade = f"{decade}s"
+    return decade
+
+df["decade"] = df["year"].apply(lambda x: year_to_decade(x))
+
+@st.cache_data
+def plot_songs_by_decade(df):
+    fig = plt.figure(figsize=(20, 8), facecolor="white")  # blank canvas with background
+    gs = fig.add_gridspec(1, 1)  # grid layout
+    ax = fig.add_subplot(gs[0, 0])  # subplot
+
+    ax.text(
+        3.5,
+        28000,
+        "Songs Released(By Decade)",
+        fontsize=25,
+        fontweight="bold",
+        fontfamily="monospace",
+    )
+
+    # Dotted horizontal gridlines
+    ax.grid(color="black", linestyle=":", axis="y", zorder=0, dashes=(1, 5))
+
+    sns.countplot(data=df, x="decade", ax=ax, alpha=1, zorder=2, color='skyblue')
+
+    # Remove border lines
+    for direction in ["top", "right", "left"]:
+        ax.spines[direction].set_visible(False)
+
+    ax.set_xlabel("Decade", fontsize=14, fontweight="bold")
+    ax.tick_params(axis="x", labelsize=14)
+    ax.tick_params(axis="y", length=0, labelsize=13)
+    ax.set_ylabel(
+        "",
+    )
+    ax.invert_xaxis()
+    return fig
 
 # Add a title
 top_prol_artists_col.write("### Top 5 Most Prolific Artists")
@@ -54,7 +118,7 @@ top_prol_artists = get_top_prolific_artists(df)
 top_prol_artists_col.table(top_prol_artists)
 
 
-shakar.image('shankar_mahadevan.jpeg', width=270)
+shakar.image('shankar_mahadevan.jpeg', width=250)
 
 @st.cache_data
 def get_top_popular_artists(df):
@@ -89,10 +153,10 @@ def plot_songs_by_language(df):
         labels=labels, 
         colors=palette_color, 
         autopct=autopct_func,
-        textprops={'fontsize': 6}
+        textprops={'fontsize': 5.5}
     )
     plt.title('Songs by Language', fontsize=10)
-    plt.text(0, -1.2, 'Remaining slices: Telagu (5%) and Malayalam (5%)', ha='right', fontsize=3.5)
+    plt.text(0, -1.2, 'Remaining slices: Telagu (5%) and Malayalam (5%)', ha='right', fontsize=4)
     plt.tight_layout() 
    
     return canv
@@ -100,66 +164,15 @@ def plot_songs_by_language(df):
 pop_artists_col.write("### Top 5 Most Popular Artists")
 pop_artists = get_top_popular_artists(df)
 pop_artists_col.table(pop_artists)
-big_dawgs.image('big_dawgs.jpeg', width=270)
+big_dawgs.image('big_dawgs.jpeg', width=250)
 
-data_stats_col, language_pie_col = st.columns(2, vertical_alignment="center", border=False)
-data_stats_col.write('### Dataset Statistics')
-total_songs = len(df)
-total_artists = df['artist_name'].nunique()
-year_range = f"{df['year'].min()} and {df['year'].max()}"
-most_songs_year = df['year'].value_counts().idxmax()
-least_songs_year = df['year'].value_counts().idxmin()
+songs_by_decade_col, language_pie_col = st.columns(2, vertical_alignment="center", border=True)
 
-data_stats_col.write(f'Total number of songs: {total_songs}')
-data_stats_col.write(f'Total number of artists: {total_artists}')
-data_stats_col.write(f'Between {year_range}')
-data_stats_col.write(f'Most songs written in {most_songs_year}, least in {least_songs_year}')
+fig = plot_songs_by_decade(df)
+songs_by_decade_col.pyplot(fig)
 
 language_pie_chart = plot_songs_by_language(df)
 language_pie_col.pyplot(language_pie_chart)
-
-@st.cache_data
-def year_to_decade(year):
-    decade = (year // 10) * 10
-    decade = f"{decade}s"
-    return decade
-
-df["decade"] = df["year"].apply(lambda x: year_to_decade(x))
-
-@st.cache_data
-def plot_songs_by_decade(df):
-    fig = plt.figure(figsize=(20, 8), facecolor="white")  # blank canvas with background
-    gs = fig.add_gridspec(1, 1)  # grid layout
-    ax = fig.add_subplot(gs[0, 0])  # subplot
-
-    ax.text(
-        3.5,
-        28000,
-        "Songs Released(By Decade)",
-        fontsize=25,
-        fontweight="bold",
-        fontfamily="monospace",
-    )
-
-    # Dotted horizontal gridlines
-    ax.grid(color="black", linestyle=":", axis="y", zorder=0, dashes=(1, 5))
-
-    sns.countplot(data=df, x="decade", ax=ax, alpha=1, zorder=2)
-
-    # Remove border lines
-    for direction in ["top", "right", "left"]:
-        ax.spines[direction].set_visible(False)
-
-    ax.set_xlabel("Decade", fontsize=14, fontweight="bold")
-    ax.tick_params(axis="x", labelsize=14)
-    ax.tick_params(axis="y", length=0, labelsize=13)
-    ax.set_ylabel(
-        "",
-    )
-    ax.invert_xaxis()
-    return fig
-
-
 
 # Tableau embed code
 tableau_html = """
@@ -172,7 +185,6 @@ st.components.v1.html(tableau_html, height=1400)
 audio_features = [
     "danceability",
     "energy",
-    "speechiness",
     "acousticness",
     "instrumentalness",
     "liveness",
@@ -268,9 +280,4 @@ for idx, (feature, col) in enumerate(zip(audio_features, cols)):
         key=f"checkbox_{feature}",
         on_change=update_feature_state,
     )
-
-
-fig = plot_songs_by_decade(df)
-st.pyplot(fig)
-
 
